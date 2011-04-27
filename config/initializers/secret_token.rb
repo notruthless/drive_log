4ea6190916abd0b7872701f7e66bd759341768af
Ruth Helfinstein <github@helfinstein.com>
2011-04-27 13:50:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DriveLog::Application.config.secret_token = '84bd785b2e53d25c3a491b4a6e254c4b092bc1a6669eb43ccc9f9cc8c238c339a74952c3c246b5c5edde2ee4a3e8e21f2c1c9127b80dcba48823f60eb439f8fd'
